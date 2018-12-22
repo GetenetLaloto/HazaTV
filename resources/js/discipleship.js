1aeb732c0d447363f8f355963b5071a72f0cd339
GetenetLaloto <getenetlaloto@gmail.com>
2018-12-21 21:59:19 -0500

    adding and deleting old datat and replacing the new one

--- resources/js/discipleship.js
+++ resources/js/discipleship.js
@@ -5,9 +5,9 @@ data_file: discipleship
 {% include partial_assets/javascript_partials.js %}
 
 $(document).ready(()=>{
-  // TODO: clean this mess. lots of repetition 
+  // TODO: clean this mess. lots of repetition
   let pdf_chosen = window.location.hash && ['first', 'second'].includes(window.location.hash.substr(1)) ? window.location.hash.substr(1) : 'first';
-  const url = `http://docs.google.com/gview?url=http://www.yegetabaryahabtamu.org/resources/pdf/${pdf_chosen}.pdf&embedded=true`
+  const url = `http://docs.google.com/gview?url=#${pdf_chosen}.pdf&embedded=true`
   $('.pdf-wrapper iframe').attr('src', url);
   $('.button-wrapper .link-button.active').removeClass('active');
   $(`.${pdf_chosen}`).addClass('active');
@@ -18,7 +18,7 @@ $(document).ready(()=>{
     const pdf_chosen = $thisButton.attr('href').substr(1);
     $('.button-wrapper .link-button.active').removeClass('active');
     $thisButton.addClass('active');
-    const url = `http://docs.google.com/gview?url=http://www.yegetabaryahabtamu.org/resources/pdf/${pdf_chosen}.pdf&embedded=true`
+    const url = `http://docs.google.com/gview?url=#${pdf_chosen}.pdf&embedded=true`
     $('.pdf-wrapper iframe').attr('src', url);
   })
 });
